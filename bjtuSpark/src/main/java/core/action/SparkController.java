6123172c11ef785ab2b5708038cbda0e55da12ef
lyahb <lyahb@vmware.com>
2016-05-26 17:55:55 +0800

    新建maven+springmvc项目

---
+++ bjtuSpark/src/main/java/core/action/SparkController.java
@@ -0,0 +1,45 @@
+package core.action;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.alibaba.fastjson.JSONObject;
+
+import core.model.SparkService;
+ 
+@Controller
+public class SparkController {
+     
+    /**
+     * @user lyahb
+     * 2014年9月4日
+     */
+    @RequestMapping(value = "/SparkServlet", method = RequestMethod.POST)
+    public void index(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        //对应的是WebContent目录下的WEB-INF目录下的jsp目录下的demo下的index.jsp
+        //请查看配置文件springMvc3-servlet.xml仔细体会一下
+    	String userId = request.getParameter("userId");
+		SparkService service = new SparkService();
+        //TODO
+		String userInfo = service.getSparkInfo(userId);
+//        System.out.println("debug +++");
+        JSONObject object = new JSONObject();
+        object.put("baseInfo",userInfo);
+        object.put("interests",userInfo);
+        object.put("profession",userInfo);
+        object.put("pagerank",userInfo);
+
+        PrintWriter out = response.getWriter();
+		out.write(object.toString());
+        out.close();  
+    }
+ 
+}
---
+++ bjtuSpark/src/main/java/core/model/SparkService.java
@@ -0,0 +1,12 @@
+package core.model;
+
+import javax.servlet.http.HttpServlet;
+
+public class SparkService {
+
+	public String getSparkInfo(String userId) {
+		//处理真正的数据，获取spark返回数据
+        return "json info";
+	}
+
+}
