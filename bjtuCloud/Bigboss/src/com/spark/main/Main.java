efbbe88de0afb759a3424584f7c77ce61f8c81dd
Liuyifan <253062115@qq.com>
2016-05-29 23:25:13 +0800

    按照信息类别模块化Main

--- bjtuCloud/Bigboss/src/com/spark/main/Main.java
+++ bjtuCloud/Bigboss/src/com/spark/main/Main.java
@@ -19,48 +19,76 @@ public class Main {
 
         String id = args[0];
 
+        JSONObject result = new JSONObject();
+
+        result.put("id", id);
+        result.put("basic", basic(id));
+        result.put("professional", profession(id));
+        result.put("interest", interest(id));
+        result.put("social", network(id));
+
+        String res = result.toString();
+        System.out.println(res);
+    }
+
+    /**
+     * 根据id,获取basic information<TODO>
+     * @param id
+     * @return
+     */
+    private static JSONObject basic(String id) {
+        JSONObject basicObj = new JSONObject();
+
 //        SparkConf conf = new SparkConf();
 //        JavaSparkContext sc = new JavaSparkContext(conf);
 //        JavaRDD<String> base = sc.textFile("/");
 
-
-        JSONObject result = new JSONObject();
-
-        // basic information
-        JSONObject basicObj = new JSONObject();
         String name = "Xiao Ming";
         String sex = "f";
         String age = "20";
         basicObj.put("name", name);
         basicObj.put("sex", sex);
         basicObj.put("age", age);
+        return basicObj;
+    }
 
-        // professional information
+    /**
+     * 根据id,获取professional information<TODO>
+     * @param id
+     * @return
+     */
+    private static JSONObject profession(String id) {
         JSONObject profObj = new JSONObject();
         String job = "hunter";
         String habit = "eat";
         profObj.put("job", job);
         profObj.put("habit", habit);
+        return profObj;
+    }
 
-        // interest
+    /**
+     * 根据id,获取interest information<TODO>
+     * @param id
+     * @return
+     */
+    private static JSONObject interest(String id) {
         JSONObject interestObj = new JSONObject();
         String interests = "music,hiking...";
         interestObj.put("interests", interests);
+        return interestObj;
+    }
 
-        // social network information
+    /**
+     * 根据id,获取social network information<TODO>
+     * @param id
+     * @return
+     */
+    private static JSONObject network(String id) {
         JSONObject socialObj = new JSONObject();
         String pageRank = "1.1";
         String group = "boss";
         socialObj.put("pageRank", pageRank);
         socialObj.put("group", group);
-
-        result.put("id", id);
-        result.put("basic", basicObj);
-        result.put("professional", profObj);
-        result.put("interest", interestObj);
-        result.put("social", socialObj);
-
-        String res = result.toString();
-        System.out.println(res);
+        return socialObj;
     }
 }
