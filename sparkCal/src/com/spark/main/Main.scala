1edc899f523fa9237d0f470ae8f7541250931b03
jiangyalynn <jiangyalynn@163.com>
2016-06-01 20:45:55 +0800

    scala

--- sparkCal/src/com/spark/main/Main.scala
+++ sparkCal/src/com/spark/main/Main.scala
@@ -12,20 +12,44 @@ import org.apache.spark.graphx.lib.LabelPropagation
 
 object Main {
   def main(args: Array[String]) {
-    //val userId = args(0)
+//	  val userId = "1035748400";
+    val userId = args(0)
     //设置运行环境
+	  if (args.length == 0) {
+		  println("No params.");
+		  sys.exit(-1);
+	  }
+//	  val userId = args(0);
+	  
+	  
     val conf = new SparkConf().setAppName("Main")
     conf.setMaster("local")
     var sc = new SparkContext(conf)
-    val userId = "1628951200"
     val baseInfo = getBasic(userId,sc)
     val interest = getInterest(userId,sc)
-    val socialInfo = getSocial(userId,sc)
-    println(interest)
-    println("social:" + socialInfo)    
+//    val socialInfo = getSocial(userId,sc)
+
+    var result : String = "";
+    result += "{\"id\": \"" + userId + "\", ";
+    result += "\"baseInfo\": \"" + baseInfo + "\", ";
+    result += "\"profession\": \"" + "test" + "\", ";
+    result += "\"interests\": \"" + interest + "\", ";
+    result += "\"pagerank\": \"" + "test" + "\"}\n";
+
+    println(result);
+
+//    val path = "/home/spark/project/result.dat"
+    val path = "/Users/lynn/Desktop/result.dat"
+
+    val fos = new FileOutputStream(path);
+    fos.write(result.getBytes);
+    fos.close();
+
+    println("Succeed!");
+    sc.stop()
   }
   def getInterest(userId: String,sc: SparkContext)= {
-    val articles: RDD[String] = sc.textFile("hdfs://localhost:9001/weibo.txt")
+    val articles: RDD[String] = sc.textFile("hdfs://spark1:9000/weibo/weibo.txt")
     
     val articles_filter =  articles.filter(a => (a.split('\t').length > 2 && a.split('\t')(1) == userId));
     var res = ""
@@ -33,20 +57,20 @@ object Main {
     res
   }
   def getBasic(userId: String,sc: SparkContext)= {
-       val articles: RDD[String] = sc.textFile("hdfs://localhost:9001/userprofile.txt")
+       val articles: RDD[String] = sc.textFile("hdfs://spark1:9000/weibo/userprofile.txt")
        val articles_filter =  articles.filter(a => (a.split('\t').length > 2 && a.split('\t')(0) == userId));
        val res = articles_filter.collect().mkString
        var basicInfo = ""
        basicInfo =  "<br/>" + res.split('\t')(1) + "<br/>" + res.split('\t')(2) + "<br/>"+res.split('\t')(3) + "<br/>"+ (if (res.split('\t')(6) == "f") '男' else '女')
        var professInfo = ""
-       TFIDF.getKeywords(res.split('\t')(12), 3).foreach(e=>professInfo=professInfo+"<br/>"+e._1)
+//       TFIDF.getKeywords(res.split('\t')(12), 3).foreach(e=>professInfo=professInfo+"<br/>"+e._1)
        // 11387	左琼	广东	广州	2009-08-27 16:00:00	2012-04-05 09:38:16	f	1	367	1858	6332	http://blog.sina.com.cn/zq4387	用你的毛，织我的脖！
        println("this is the info of basic " + basicInfo)
-       println("this is the info of profess " + professInfo)
+//       println("this is the info of profess " + professInfo)
        basicInfo
   }
   def getSocial(userId: String,sc: SparkContext) = {
-    val relationFile = "hdfs://localhost:9001/relation.txt"
+    val relationFile = "hdfs://spark1:9000/weibo/relation.txt"
     //load graph
     val graph = GraphLoader.edgeListFile(sc, relationFile)
     //Calculate page rank and save the result to VertexRDD
